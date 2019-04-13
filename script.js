d39f6c6b56ceb424c37095accb13982c36e9ec84
Shaun Sproston <shaunsproston@yahoo.co.uk>
2019-04-13 17:23:57 +0100

    Addition functionality

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const btn = document.getElementsByClassName("btn");
 
 // Keypad Controls and Cancel
 
+
+
 document.addEventListener("click", function(e){
   let inputVal = e.target.innerHTML;
   if (inputVal === "C"){
@@ -15,11 +17,35 @@ document.addEventListener("click", function(e){
   }
 });
 
-// Addition
+// Simple addition
 
 document.addEventListener("click", function(e){
   let inputVal = e.target.innerHTML;
   if (inputVal === "="){
-    console.log(input.innerHTML.slice(0,-1));
+    inputVal = input.innerHTML.slice(0,-1);
+    let result = eval(inputVal);
+    output.innerHTML = result;
+    input.innerHTML = "";
+    console.log("test 2 " + result);
   }
 })
+
+
+/*
+switch (inputVal) {
+  case 1:
+    console.log(inputVal);
+    break;
+  case 2:
+    console.log('Number ok');
+    break;
+  case 3:
+    console.log('Number ok');
+    break;
+  case 4:
+    console.log('Number ok');
+    break;
+  default:
+    console.log('Invalid input');
+}
+*/
