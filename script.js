fdc33b49edba4bf8df737bad867b9ef0105c2209
Shaun Sproston <shaunsproston@yahoo.co.uk>
2019-04-14 22:15:05 +0100

    Work in progress

--- script.js
+++ script.js
@@ -2,50 +2,41 @@
 
 const input = document.getElementById("input");
 let output = document.getElementById("output");
-const btn = document.getElementsByClassName("btn");
+let btn = document.getElementsByClassName("btn");
+console.log(btn[6].innerHTML);
+
+
+
 
 // Keypad Controls and Cancel
 
-
-
 document.addEventListener("click", function(e){
   let inputVal = e.target.innerHTML;
+
   if (inputVal === "C"){
     input.innerHTML = "";
   } else {
     input.innerHTML += e.target.innerHTML;
+    console.log('test 2 ' + inputVal)
   }
-});
-
-// Simple addition
-
-document.addEventListener("click", function(e){
-  let inputVal = e.target.innerHTML;
   if (inputVal === "="){
     inputVal = input.innerHTML.slice(0,-1);
     let result = eval(inputVal);
     output.innerHTML = result;
     input.innerHTML = "";
-    console.log("test 2 " + result);
+    console.log('test 3 ' + inputVal)
   }
-})
 
-
+});
 /*
-switch (inputVal) {
-  case 1:
-    console.log(inputVal);
-    break;
-  case 2:
-    console.log('Number ok');
-    break;
-  case 3:
-    console.log('Number ok');
-    break;
-  case 4:
-    console.log('Number ok');
-    break;
-  default:
-    console.log('Invalid input');
-}
+document.addEventListener("click", function(e){
+  let inputVal = e.target.innerHTML;
+
+  if (inputVal === "1"){
+    input.innerHTML = "1";
+  } else {
+    input.innerHTML = ""
+  }
+
+});
 */
